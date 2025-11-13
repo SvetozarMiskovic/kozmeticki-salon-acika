05f12a61e50c606bd8c6e367b77736bf3068fe27
SvetozarMiskovic <svetomiskovic98@gmail.com>
2025-11-13 17:04:19 +0100

    added redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,17 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 import icon from 'astro-icon';
+import { defaultLanguage } from './src/i18n/ui';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://salonacika.appwrite.network',
+  site: 'https://salonacika.appwrite.network/',
+  redirects: {
+    '/': `${defaultLanguage}`,
+  },
   i18n: {
     defaultLocale: 'sr',
     routing: {
