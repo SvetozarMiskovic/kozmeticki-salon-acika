9999bad6a3c447db5ab01abc70df4df60e63e038
SvetozarMiskovic <svetomiskovic98@gmail.com>
2025-11-14 17:04:05 +0100

    added links

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://salonacika.com/',
+  site: 'http://localhost:4321',
   redirects: {
     '/': `${defaultLanguage}`,
   },
